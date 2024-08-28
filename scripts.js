8c125961fbf0a62b62518c93917830ec0bb3f952
norma13115 <normapadilla13115@gmail.com>
2024-08-28 01:18:05 +0000

    updated error code on html

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
-console.log ("My console log works!!!!!!")
+//calculated and return the total for a sale
+
+//definition of the funtion
+function saleTotal (item1, item2, item3){
+    return item1 + item2 + item3 
+}
+
+// call of the function
+console.log ( saleTotal (2.99, 5.25, 4.99)) //13.23
+
+//function to calculate area of rectangle
+//definition of the function
+function areaRectangle (length, width){
+    return length * width 
+}
+//call function
+console.log (areaRectangle(10, 20))
+
+//funciton to divide two numbers
+function divide (item1, item2){
+    return item1 / item2
+}
+console.log (divide(100, 10))
